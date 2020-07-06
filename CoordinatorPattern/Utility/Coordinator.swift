cdd92398b7754a70a23c9e2c3d914c16e4ed9029
Varun Rathi <varun.rathi@jeevansathi.com>
2020-07-07 02:49:24 +0530

    folder restructure

--- CoordinatorPattern/Utility/Coordinator.swift
+++ CoordinatorPattern/Utility/Coordinator.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-protocol  Coordinator {
+protocol  Coordinator :class{
     var childCoordinators: [Coordinator] {  get set }
     var navigationController:UINavigationController { get set}
     
     func start()
 }
+
